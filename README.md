3ec2edd3c0f90593459cc822f6da04e3dadff129
Brian Bancroft <bancroft.bw@gmail.com>
2015-05-09 18:17:45 -0700

    Initial Change
    I've added a webmap.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # brian2073.github.io
-My internet space
+My internet space. 
+
+Currently, I am using github as a means to prototype a half-decent leaflet map as an Open Data
+reference map project. 
---
+++ webmap-1/anim.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$(document).ready(function () {
+    $('#title').click(function () {
+        $(this).css('background-color', 'green');
+    });
+
+    $('li#instr').click(function () {
+        $(this).css('background-color', 'orange');
+    });
+    $('li#info').click(function () {
+        $(this).css('background-color', 'orange');
+    });
+
+    $('#legendTab').click(function () {
+        $(this).css('background-color', 'orange');
+    });
+
+});
