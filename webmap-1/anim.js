1c23fa358e6d955fc4a3c2c89026979d92cb6866
Brian Bancroft <bancroft.bw@gmail.com>
2015-05-19 15:03:07 -0700

    General work
    Played with JQuery. made things animate.

--- webmap-1/anim.js
+++ webmap-1/anim.js
@@ -5,14 +5,37 @@ $(document).ready(function () {
     });
 
     $('li#instr').click(function () {
-        $(this).css('background-color', 'orange');
+        $(this).css('visibility','hidden');
+		$('li#info').css('visibility','hidden');
+		$('#instrPanel').css('visibility','visible');
     });
     $('li#info').click(function () {
-        $(this).css('background-color', 'orange');
+        $(this).css('visibility','hidden');
+		$('li#instr').css('visibility','hidden');
+		$('#infoPanel').css('visibility','visible');
     });
+	
+	$('#instrPanel').click(function(){
+		$(this).css('visibility','hidden');
+		$('li#info').css('visibility','visible');
+		$('li#instr').css('visibility','visible');
+	
+	});
+	$('#infoPanel').click(function(){
+		$(this).css('visibility','hidden');
+		$('li#info').css('visibility','visible');
+		$('li#instr').css('visibility','visible');
+	
+	});
+	
+	$('#legendPanel').click(function(){
+		$(this).css('visibility','hidden');
+		$('#legendTab').css('visibility','visible');
+	});
 
     $('#legendTab').click(function () {
-        $(this).css('background-color', 'orange');
+        $(this).css('visibility', 'hidden');
+		$('#legendPanel').css('visibility','visible');
     });
 
 });
