c9ad0bed0b3a79af7d6ac86a78d8dc1e308072d7
Brian Bancroft <hello@brianbancroft.ca>
2016-03-17 21:17:15 -0400

    test

--- Recipie-Page/script.js
+++ Recipie-Page/script.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-
-  console.log("Script included!");
-
-
-
+	$('#imageShow').hide();
 	$('#ingredients ul li').click(function()
 	{
-		console.log("Clicked")
 		$(this).toggleClass("checked")
 
 	});
+	$('img').click(function(){
+	});
+	$('#imageHide').click(function(){
+		$('img').toggle();
+		$('#imageHide').hide();
+		$('#imageShow').show();
+	});
+	$('#imageShow').click(function(){
+		$('img').toggle();
+		$('#imageHide').show();
+		$('#imageShow').hide();
+	})
+	$('ol li').click(function(){
+		$('ol li').css('background-color',"#eee");
+		$(this).css('background-color',"orange")
+
+	})
 
 });
