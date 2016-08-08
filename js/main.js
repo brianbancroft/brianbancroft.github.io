453391c9a4a50ebd145bdfebae714b489f0f4b23
Brian Bancroft <hello@brianbancroft.ca>
2016-08-08 13:13:36 -0400

    background image removed and redirect through button click on front page

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ $(function() {
     // Code here
     $('.left-page').click(function () {
       console.log('left page clicked')
+      $('body').css('background-image','none')
+      window.location.href = '/portfolio'
     })
     $('.right-page').click(function () {
       console.log('right page clicked')
+      window.location.href = '/posts'
     })
 })
