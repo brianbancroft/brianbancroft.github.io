ac6b20eef9dbf830d4b08da2849e11f85f6027ab
Brian Bancroft <hello@brianbancroft.ca>
2016-08-08 13:07:54 -0400

    front page event handlers are connected

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function() {
     // Code here
-});
+    $('.left-page').click(function () {
+      console.log('left page clicked')
+    })
+    $('.right-page').click(function () {
+      console.log('right page clicked')
+    })
+})
