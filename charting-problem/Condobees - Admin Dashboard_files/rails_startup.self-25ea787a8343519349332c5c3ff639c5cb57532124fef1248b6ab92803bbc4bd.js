c3a89e1e0f6b8359061c12e72745e5d2dde73672
Brian Bancroft <hello@brianbancroft.ca>
2017-01-25 14:25:21 -0500

    removal of extra files

--- charting-problem/Condobees - Admin Dashboard_files/rails_startup.self-25ea787a8343519349332c5c3ff639c5cb57532124fef1248b6ab92803bbc4bd.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function () {
-  // highlight active page in the top menu
-  // console.log('We are active');
-  // if (typeof Uint8ClampedArray == "undefined") {
-  //   Uint8ClampedArray = Uint8Array;
-  // }
-});
