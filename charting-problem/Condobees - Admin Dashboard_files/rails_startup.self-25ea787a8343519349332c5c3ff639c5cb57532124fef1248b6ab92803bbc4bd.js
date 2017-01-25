50e7b0839f7afd4b7d823ca62fd4320a5284c134
Brian Bancroft <hello@brianbancroft.ca>
2017-01-25 14:24:28 -0500

    updated cv

---
+++ charting-problem/Condobees - Admin Dashboard_files/rails_startup.self-25ea787a8343519349332c5c3ff639c5cb57532124fef1248b6ab92803bbc4bd.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function () {
+  // highlight active page in the top menu
+  // console.log('We are active');
+  // if (typeof Uint8ClampedArray == "undefined") {
+  //   Uint8ClampedArray = Uint8Array;
+  // }
+});
